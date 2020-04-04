107fb115a02cd6e62e15c285df59d8f50bf9cce3
rajeev <rajeev29five@gmail.com>
2020-04-05 00:57:50 +0530

    quotes added

--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -10,6 +10,7 @@ class RandomQuote extends Component
             quote : '',
             author : ''
         }
+        this.getNewQuote = this.getNewQuote.bind(this)
     }
 
     componentDidMount () {
@@ -17,7 +18,7 @@ class RandomQuote extends Component
     }
 
     getQuote() {
-        const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+        const url = "https://raw.githubusercontent.com/rajeev29five/animequote/master/src/static/quotes.json"
         axios.get(url)
              .then(res => {
                  
@@ -32,7 +33,7 @@ class RandomQuote extends Component
              }) 
     }
 
-    getNewQuote() {
+    getNewQuote(){
         this.getQuote()
     }
 
@@ -40,10 +41,16 @@ class RandomQuote extends Component
         const {quote, author} = this.state
         return (
             <div id='wrapper'>
-                <h1>Random Quote App</h1>
+                <h1>Anime Quote</h1>
                 <div>
                     <p>{quote}</p>
                     <h5>{author}</h5>
+                </div>
+                <div id="buttons">
+                    <a href={`https://twitter.com/intent/tweet?text=${quote} \n ${author}`} target='_blank' title="Post this quote on twitter!">
+                        Tweet this quote.
+                    </a>
+                    <button id="new-quote" onClick={this.getNewQuote}>New Quote</button>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import RandomQuote from './components/RandomQuote';
 
