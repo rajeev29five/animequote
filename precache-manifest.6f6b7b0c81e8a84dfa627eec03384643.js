6237199fe2a4c1da55369b1cee1b0de5aeb27b24
rajeev <rajeev29five@gmail.com>
2020-04-06 01:07:24 +0530

    Updates

---
+++ precache-manifest.6f6b7b0c81e8a84dfa627eec03384643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802208f15fc9cde46fec3ed7ad2e03ba",
+    "url": "/animequote/index.html"
+  },
+  {
+    "revision": "4bb9394cfe4a53993f2f",
+    "url": "/animequote/static/css/main.24012418.chunk.css"
+  },
+  {
+    "revision": "a2321fa3e14fcd0dc17c",
+    "url": "/animequote/static/js/2.9763a25e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animequote/static/js/2.9763a25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb9394cfe4a53993f2f",
+    "url": "/animequote/static/js/main.833da4d7.chunk.js"
+  },
+  {
+    "revision": "852195deab9f94af4e7f",
+    "url": "/animequote/static/js/runtime-main.174638b2.js"
+  }
+]);
